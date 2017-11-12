41313fbdd0e7b74f9cfabf0b2fa9ca926ab35676
Valentin Sundermann <me@vsund.de>
2017-11-12 15:03:56 +0100

    Simplify error with super() call
    http://azimi.me/2015/09/23/high-custom-error-class-es6.html

--- lib/ParseError.ts
+++ lib/ParseError.ts
@@ -8,17 +8,13 @@ export class ParseError extends Error {
 	/** The error message */
 	message: string;
 
-	/** Stack trace */
-	stack: string|undefined;
-
 	/** Constructor for creating a new parse error */
 	constructor(attribute?: string) {
-		super();
+		super(attribute);
 
 		this.name = 'ParseError';
 		this.message = attribute !== undefined ?
 			'Could not parse attribute "' + attribute + '" retrieved html' :
 			'Could not parse retrieved html';
-		this.stack = (new Error()).stack;
 	}
 }
